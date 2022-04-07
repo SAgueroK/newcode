22e3baf0992e08e9b55d4fbe41721e1d0e9bf37d
codefish18 <1912607519@qq.com>
2022-04-07 15:39:44 +0800

    添加了字典树过滤器

---
+++ src/main/java/com/util/SensitiveFilter.java
@@ -0,0 +1,149 @@
+package com.util;
+
+
+import org.apache.commons.lang3.CharUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class SensitiveFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(SensitiveFilter.class);
+
+    // 替换符
+    private static final String REPLACEMENT = "***";
+
+    // 根节点
+    private TrieNode rootNode = new TrieNode();
+
+    @PostConstruct
+    public void init() {
+        try (
+                InputStream is = this.getClass().getClassLoader().getResourceAsStream("sensitive-words.txt");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        ) {
+            String keyword;
+            while ((keyword = reader.readLine()) != null) {
+                // 添加到前缀树
+                this.addKeyword(keyword);
+            }
+        } catch (IOException e) {
+            logger.error("加载敏感词文件失败: " + e.getMessage());
+        }
+    }
+
+    // 将一个敏感词添加到前缀树中
+    private void addKeyword(String keyword) {
+        TrieNode tempNode = rootNode;
+        for (int i = 0; i < keyword.length(); i++) {
+            char c = keyword.charAt(i);
+            TrieNode subNode = tempNode.getSubNode(c);
+
+            if (subNode == null) {
+                // 初始化子节点
+                subNode = new TrieNode();
+                tempNode.addSubNode(c, subNode);
+            }
+
+            // 指向子节点,进入下一轮循环
+            tempNode = subNode;
+
+            // 设置结束标识
+            if (i == keyword.length() - 1) {
+                tempNode.setKeywordEnd(true);
+            }
+        }
+    }
+
+    /**
+     * 过滤敏感词
+     *
+     * @param str 待过滤的文本
+     * @return 过滤后的文本
+     */
+    public String filter(String str) {
+        int begin = 0;
+        int position = 0;
+        StringBuilder res = new StringBuilder();
+        TrieNode now = rootNode;
+        if (rootNode == null) {
+            return str;
+        }
+        while (begin < str.length()) {
+            if (position < str.length()) {
+                char c = str.charAt(position);
+                if (!Character.isLetter(c)) {
+                    position++;
+                    continue;
+                }
+                now = now.getSubNode(c);
+                if (now == null) {//如果为空，就是没有敏感词的一个子串，说明begin开头的肯定不是敏感词，把begin加上
+                    res.append(str.charAt(begin));
+                    begin++;
+                    position = begin;
+                    now = rootNode;
+                    continue;
+                }
+                if (now.isKeywordEnd) {//并且是关键字
+                    begin = ++position;
+                    res.append(REPLACEMENT);
+                    now = rootNode;
+                } else {
+                    position++;
+                }
+            } else {
+                res.append(str.charAt(begin));
+                begin++;
+                position = begin;
+                now = rootNode;
+            }
+        }
+        return res.toString();
+    }
+
+    // 判断是否为符号
+    private boolean isSymbol(Character c) {
+        // 0x2E80~0x9FFF 是东亚文字范围
+        return !CharUtils.isAsciiAlphanumeric(c) && (c < 0x2E80 || c > 0x9FFF);
+    }
+
+    // 前缀树
+    private class TrieNode {
+
+        // 关键词结束标识
+        private boolean isKeywordEnd = false;
+
+        // 子节点(key是下级字符,value是下级节点)
+        private Map<Character, TrieNode> subNodes = new HashMap<>();
+
+        public boolean isKeywordEnd() {
+            return isKeywordEnd;
+        }
+
+        public void setKeywordEnd(boolean keywordEnd) {
+            isKeywordEnd = keywordEnd;
+        }
+
+        // 添加子节点
+        public void addSubNode(Character c, TrieNode node) {
+            subNodes.put(c, node);
+        }
+
+        // 获取子节点
+        public TrieNode getSubNode(Character c) {
+            return subNodes.get(c);
+        }
+
+    }
+
+}
--- src/test/java/com/daomian.java
+++ src/test/java/com/daomian.java
@@ -1,2 +1,19 @@
-package com;public class daomian {
+package com;
+
+import com.util.SensitiveFilter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class daomian {
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+    @Test
+    public void test(){
+        sensitiveFilter.init();
+        String s=sensitiveFilter.filter("ds开票d");
+        System.out.println(s);
+    }
+
 }
